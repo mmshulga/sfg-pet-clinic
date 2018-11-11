52b63208bc89022d65a8b5cf876e7e852fdd0286
Mikhail Shulga <mmshulga@gmail.com>
2018-11-11 23:15:22 +0300

    Refactored to common crud interface.

--- pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/PetService.java
@@ -2,10 +2,5 @@ package my.mmshulga.sfgpetclinic.services;
 
 import my.mmshulga.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package my.mmshulga.sfgpetclinic.services;
 
 import my.mmshulga.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
