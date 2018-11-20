10a77bf425fafc537f2d6c92d26c6d4ea1bc0adb
Mikhail Shulga <mmshulga@gmail.com>
2018-11-20 10:53:51 +0300

    Implemented jpa-based service for owners.

---
+++ pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,57 @@
+package my.mmshulga.sfgpetclinic.services.springdatajpa;
+
+import my.mmshulga.sfgpetclinic.model.Owner;
+import my.mmshulga.sfgpetclinic.repositories.OwnerRepository;
+import my.mmshulga.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("sdj")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    @Autowired
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        Optional<Owner> owner = ownerRepository.findById(id);
+        return owner.orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
