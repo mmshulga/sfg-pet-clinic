6962f272185edea906a1223df7ecc12fa94591db
Mikhail Shulga <mmshulga@gmail.com>
2018-11-20 11:10:21 +0300

    Implemented jpa-based pet service.

---
+++ pet-clinic-data/src/main/java/my/mmshulga/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,50 @@
+package my.mmshulga.sfgpetclinic.services.springdatajpa;
+
+import my.mmshulga.sfgpetclinic.model.Pet;
+import my.mmshulga.sfgpetclinic.repositories.PetRepository;
+import my.mmshulga.sfgpetclinic.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile({"sdj", "default"})
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Autowired
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> all = new HashSet<>();
+        petRepository.findAll().forEach(all::add);
+        return all;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
