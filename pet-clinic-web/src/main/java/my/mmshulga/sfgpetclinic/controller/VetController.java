d5c4b3f961b7ba4d5773c3d0b563801e5f0368ce
Mikhail Shulga <mmshulga@gmail.com>
2018-11-29 11:59:35 +0300

    Now you can get json version of a vets list.

--- pet-clinic-web/src/main/java/my/mmshulga/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/my/mmshulga/sfgpetclinic/controller/VetController.java
@@ -1,12 +1,15 @@
 package my.mmshulga.sfgpetclinic.controller;
 
+import my.mmshulga.sfgpetclinic.model.Vet;
 import my.mmshulga.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/vets")
+import java.util.Set;
+
 @Controller
 public class VetController {
 
@@ -17,9 +20,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
     public String list(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
